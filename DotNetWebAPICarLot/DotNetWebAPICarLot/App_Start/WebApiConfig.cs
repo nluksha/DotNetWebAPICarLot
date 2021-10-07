218d85f7aa6b200b00b695009341f66752e937a5
Nikita Luksha <nikita.luksha@softensity.com>
2021-10-07 10:02:35 +0300

    Add Controller

--- DotNetWebAPICarLot/DotNetWebAPICarLot/App_Start/WebApiConfig.cs
+++ DotNetWebAPICarLot/DotNetWebAPICarLot/App_Start/WebApiConfig.cs
@@ -14,11 +14,13 @@ namespace DotNetWebAPICarLot
             // Web API routes
             config.MapHttpAttributeRoutes();
 
+            /*
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+            */
         }
     }
 }

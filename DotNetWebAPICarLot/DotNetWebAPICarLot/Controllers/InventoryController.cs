c477fb1ccb9287f8bd2f3005f2d61ba9238bf987
Nikita Luksha <nikita.luksha@softensity.com>
2021-10-08 06:50:39 +0300

    Add controller

---
+++ DotNetWebAPICarLot/DotNetWebAPICarLot/Controllers/InventoryController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace DotNetWebAPICarLot.Controllers
+{
+    [RoutePrefix("api/Inventory")]
+    public class InventoryController : ApiController
+    {
+        [HttpGet, Route("")]
+        public IEnumerable<string> Get()
+        {
+            return new [] { "value1", "value2" };
+        }
+
+        [HttpGet, Route("{id}")]
+        public string Get(int id)
+        {
+            return id.ToString();
+        }
+    }
+}

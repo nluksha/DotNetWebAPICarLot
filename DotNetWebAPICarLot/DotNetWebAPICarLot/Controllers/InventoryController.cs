195672b5a18f1d59f75c2296011eab5511c476ba
Nikita Luksha <nikita.luksha@softensity.com>
2021-10-11 19:18:18 +0300

    Add api for inventory

--- DotNetWebAPICarLot/DotNetWebAPICarLot/Controllers/InventoryController.cs
+++ DotNetWebAPICarLot/DotNetWebAPICarLot/Controllers/InventoryController.cs
@@ -1,17 +1,22 @@
 ﻿using AutoMapper;
 using DotNetEFAutoLot.DAL.Models;
+using DotNetEFAutoLot.DAL.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
+using System.Web.Http.Description;
 
 namespace DotNetWebAPICarLot.Controllers
 {
     [RoutePrefix("api/Inventory")]
     public class InventoryController : ApiController
     {
-        private Mapper mapper;
+        private readonly Mapper mapper;
+        private readonly InventoryRepository inventoryRepository = new InventoryRepository();
 
 
         public InventoryController()
@@ -24,6 +29,57 @@ namespace DotNetWebAPICarLot.Controllers
         }
 
         [HttpGet, Route("")]
+        public IEnumerable<Inventory> GetInventory()
+        {
+            var list = inventoryRepository.GetAll();
+            var inventories = mapper.Map<List<Inventory>, List<Inventory>>(list);
+
+            return inventories;
+        }
+
+        [HttpGet, Route("{id}", Name = "DisplayRoute")]
+        public async Task<IHttpActionResult> GetInventory(int id)
+        {
+            var inventory = inventoryRepository.GetOne(id);
+
+            if (inventory == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(mapper.Map<Inventory, Inventory>(inventory));
+        }
+
+        [HttpPut, Route("{id}")]
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutInventory(int id, Inventory inventory)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != inventory.Id)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                inventoryRepository.Save(inventory);
+
+            }
+            catch(Exception ex)
+            {
+                // todo: handel exception
+                throw;
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        /*
+        [HttpGet, Route("")]
         public IEnumerable<string> Get()
         {
             return new [] { "value1", "value2" };
@@ -33,6 +89,17 @@ namespace DotNetWebAPICarLot.Controllers
         public string Get(int id)
         {
             return id.ToString();
+        }
+        */
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                inventoryRepository.Dispose();
+            }
+
+            base.Dispose(disposing);
         }
     }
 }
--- DotNetWebAPICarLot/DotNetWebAPICarLot/Global.asax.cs
+++ DotNetWebAPICarLot/DotNetWebAPICarLot/Global.asax.cs
@@ -1,5 +1,7 @@
+using DotNetEFAutoLot.DAL.EF;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
@@ -12,6 +14,7 @@ namespace DotNetWebAPICarLot
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+            Database.SetInitializer(new MyDataInitializer());
         }
     }
 }
